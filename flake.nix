fa13ddbe578074c603fbddfb8cc502cbcacc11fb
fagurativejam <fagurativejam@proton.me>
2026-08-02 19:29:12 -0500

    idk

--- flake.nix
+++ flake.nix
@@ -17,20 +17,14 @@
           version = "1.0.0";
           src = ./.; # Automatically packages your main.lua and conf.lua files
 
-          # FIXED BUILD TRACK: Registering the font package so Nix can fetch its assets
-          buildInputs = [ pkgs.nerd-fonts.jetbrains-mono ];
           nativeBuildInputs = [ pkgs.makeWrapper ];
 
           installPhase = ''
             # Create isolated target destination storage trees inside the Nix store
             mkdir -p $out/share/mango-calendar $out/bin
             
-            # Copy all project source files cleanly into the store directory path
+            # Copy all files cleanly into the store directory path
             cp -r ./* $out/share/mango-calendar/
-
-            # FIXED BUNDLING: Copies the true Regular font file out of your nixpkgs derivation 
-            # and drops it directly into the application store folder layout as 'font.ttf'!
-            cp "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/JetBrainsMonoNerdFont-Regular.ttf" $out/share/mango-calendar/font.ttf
 
             # Generates a wrapper script binary that hooks love directly to the assets!
             makeWrapper ${pkgs.love}/bin/love $out/bin/mango-calendar \
